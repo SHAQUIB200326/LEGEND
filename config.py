49ef7c545577824a8468657e91430baa520da021
SHAQUIB200326 <legacyxspoofed@outlook.com>
2024-11-17 18:59:39 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("6339953677:AAFO81xLnxG1VoNgX_AYZo1kHA4VFJ9Fk5A")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@legacy4real0")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1342302666")
+
+
+
+
+
+
+
+  
